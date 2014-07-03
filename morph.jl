005354d3b094427e5a9d4620743bc38f1d0b726a
Ethan Sherbondy <sherbondye@gmail.com>
2014-07-02 20:29:05 -0400

    untested spiral sampling function.

--- morph.jl
+++ morph.jl
@@ -67,3 +67,59 @@ end
 primary_source = convert_to_primaries(source, primaries)
 
 colorim(primary_source, "RGBA")
+# now we radiate out... and stop at the closest pixel in dest with the same color as the pixel in source
+# this, too, is massively parallel, since all we need to do is read, and we do it for every pixel
+# need to compute for both source to dest and dest to source.
+# we are going to travel along a discretized archemedian spiral:
+# http://cnr.lwlss.net/RasterArchimedes/
+
+function spiral_point(theta, alpha)
+    int((alpha*theta/(2*pi))*[cos(theta), sin(theta)])
+end
+
+function delta(theta, alpha, omega)
+    (2*pi*omega)/sqrt(alpha^2*(1+theta^2))
+end
+
+circle_location(1/sqrt(2), 1/sqrt(2))
+function distance(a,b)
+    diff = a-b
+    sqrt(dot(diff,diff))
+end
+
+function get_next_point(theta, alpha, omega, focus_point)
+    # return tuple containing next sample point [x,y] AND next theta
+    delta_new = delta(theta, alpha, omega)
+    theta_new = theta + delta_new
+    (focus_point + spiral_point(theta_new, alpha), theta_new)
+end
+
+function in_rectangle(point, top_left, bottom_right)
+    top_left[1] <= point[1] <= bottom_right[1] && top_left[2] <= point[2] <= bottom_right[2]
+end
+
+function sample_spiral(focus_point, halt_fn, top_left, bottom_right)
+    # Sample pixels along a spiral radiating out from `focus_point`
+    # along rectangular grid bounded by `top_left` and `bottom_right`
+    # halting on the condition that `halt_fn` returns true for
+    # the current point.
+    # Algorithm based on: http://cnr.lwlss.net/RasterArchimedes/
+    alpha = 1/sqrt(2)
+    omega = 1/sqrt(2)
+    theta = 0
+
+    corners = [[top_left[1], top_left[2]], [bottom_right[1], top_left[2]],
+               [bottom_right[1], bottom_right[2]], [top_left[1], bottom_right[2]]]
+    distances = map(corner -> distance(focus_point, corner), corners)
+    farthest_corner = corners[indmax(distances)]
+
+    # initialize current x, y
+    current_point = focus_point
+    while current_point != farthest_corner
+        current_point, theta = get_next_point(theta, alpha, omega, focus_point)
+        if in_rectangle(current_point, top_left, bottom_right) && halt_fn(current_point)
+            return current_point
+        end
+    end
+    false
+end
